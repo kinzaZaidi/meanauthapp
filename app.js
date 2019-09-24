e912a599c3ba9bc59b312ea14b3fecbaa020294c
Kaygees - kinza <kinzaidi@gmail.com>
2019-09-24 17:31:24 +0500

    initial commit

--- app.js
+++ app.js
@@ -4,9 +4,18 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const passport = require('passport');
 const mongoose = require('mongoose');
+const config = require('./config/database');
 
-mongoose.connect();
+// connect to database
+mongoose.connect(config.database);
+mongoose.connection.on('connected', () =>{
+    console.log(`connected to database ${config.database}`);
+});
 
+// on error
+mongoose.connection.on('error',(err) =>{
+console.log(`database errot ${err}`);
+});
 const app = express();
 
 const users = require('./routes/users');
@@ -18,7 +27,13 @@ app.use(cors());
 // set static folder
 app.use(express.static(path.join(__dirname,'public')));
 
+
 app.use(bodyParser.json());
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./config/passport')(passport);
 
 app.use('/users',users);
 
